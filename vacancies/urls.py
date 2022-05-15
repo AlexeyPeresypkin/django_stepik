8f1daf35964fa300b35e0f2d40a6960d51cc2c5a
Alexey Peresypkin <aepre3@gmail.com>
2022-05-15 13:52:39 +0300

    Created VacancyBySpecialtyListView

--- vacancies/urls.py
+++ vacancies/urls.py
@@ -1,12 +1,31 @@
 from django.urls import path
 
-from vacancies.views import cat_view, companie_view, \
-    vacancie_view, IndexView, VacancyListView
+from vacancies.views import companie_view, \
+    vacancie_view, IndexView, VacancyListView, VacancyBySpecialtyListView
 
 urlpatterns = [
-    path('', IndexView.as_view(), name='index'),
-    path('vacancies', VacancyListView.as_view(), name='vacancies'),
-    path('vacancies/cat/frontend', cat_view, name='cat_view'),
-    path('companies.json/345', companie_view, name='companie_view'),
-    path('vacancies/22', vacancie_view, name='vacancie_view'),
+    path(
+        '',
+        IndexView.as_view(),
+        name='index'),
+    path(
+        'vacancies',
+        VacancyListView.as_view(),
+        name='vacancies'
+    ),
+    path(
+        'vacancies/cat/<specialty>',
+        VacancyBySpecialtyListView.as_view(),
+        name='vacancy_by_specialty'
+    ),
+    path(
+        'companies.json/345',
+        companie_view,
+        name='companie_view'
+    ),
+    path(
+        'vacancies/22',
+        vacancie_view,
+        name='vacancie_view'
+    ),
 ]
--- vacancies/views.py
+++ vacancies/views.py
@@ -1,5 +1,5 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.views.generic import ListView
 
 from vacancies.models import Vacancy, Specialty, Company
@@ -9,24 +9,39 @@ class IndexView(ListView):
     model = Company
     template_name = 'index.html'
     context_object_name = 'companies'
+    queryset = Company.objects.prefetch_related('vacancies')
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['specialties'] = Specialty.objects.all()
+        context['specialties'] = Specialty.objects. \
+            prefetch_related('vacancies')
         return context
 
 
 class VacancyListView(ListView):
-    model = Vacancy
     template_name = 'vacancies.html'
     context_object_name = 'vacancies'
     queryset = Vacancy.objects. \
         select_related('specialty'). \
         select_related('company')
+    extra_context = {'title': 'Все вакансии'}
 
 
-def cat_view(request):
-    return HttpResponse("cat_view.")
+class VacancyBySpecialtyListView(ListView):
+    template_name = 'vacancies.html'
+    context_object_name = 'vacancies'
+    # extra_context = self.get_specialty()
+
+    def get_queryset(self):
+        self.specialty = get_object_or_404(Specialty,
+                                           code=self.kwargs['specialty'])
+        return Vacancy.objects.filter(specialty=self.specialty). \
+            select_related('specialty'). \
+            select_related('company')
+
+    def get_specialty(self):
+        return {'specialty': get_object_or_404(Specialty,
+                                           code=self.kwargs['specialty'])}
 
 
 def companie_view(request):
